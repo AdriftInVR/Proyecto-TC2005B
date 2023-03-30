f0a331408514a2726aa5c401ee27417e271f8f73
Osvaldo Del Valle <a01275702@tec.mx>
2023-03-30 10:58:51 -0600

    a little

--- models/tickets.model.js
+++ models/tickets.model.js
@@ -5,5 +5,8 @@ module.exports = class Project {
     static fetchAll() {
         return db.execute('SELECT * FROM ticket');
      }
-
+    
+    static add(data){
+        db.execute('INSERT INTO TICKET VALUES ?',data)        
+    }
 }
