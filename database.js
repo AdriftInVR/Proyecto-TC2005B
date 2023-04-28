076a35c4a5f6015695f44213d562232895b0e472
sen <inarrituzanetti@outlook.com>
2023-04-28 11:18:38 -0600

    lista rama deploy

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'gaia',
-    password: 'BD_PASS',
+    password: BD_PASS,
 });
 
 module.exports = pool.promise();
--- util/database.js
+++ util/database.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
-
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', 
     database: 'gaia', 
-    password: 'BD_PASS',
+    password: BD_PASS,
 });
 
 module.exports = pool.promise();
