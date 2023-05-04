ae514efc0877cd9dc02bae54270fd66a14d08cc5
sen <inarrituzanetti@outlook.com>
2023-05-04 12:19:19 -0600

    cambio orden post get

--- routes/gaia.routes.js
+++ routes/gaia.routes.js
@@ -14,12 +14,12 @@ router.get('/', auth, gaiaController.getProjects);
 router.get('/project/:prj', auth, gaiaController.getProject);
 router.get('/tasks/:prj', auth, gaiaController.getTasks);
 router.get('/users', auth, gaiaController.getUsers);
-router.post('/users', auth, gaiaController.postDeleteUsers);
 router.get('/dashboard', auth, gaiaController.getDashboard);
 router.get('/import/:result', auth, gaiaController.getImport);
 router.get('/deletePrj/:id', auth, gaiaController.getDeletePrj)
 
 //Post routes
+router.post('/users', auth, gaiaController.postDeleteUsers);
 router.post('/import',auth,gaiaController.postImport);
 router.post('/',auth,gaiaController.postProject);
 router.post('/project/:prj', auth, gaiaController.postEditProject)
