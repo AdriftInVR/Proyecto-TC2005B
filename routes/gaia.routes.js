058e37e4476f2931d918cf4d76650225565c7b1a
Osvaldo Del Valle <a01275702@tec.mx>
2023-05-03 19:15:35 -0600

    Fix tasks

--- routes/gaia.routes.js
+++ routes/gaia.routes.js
@@ -13,7 +13,6 @@ router.get('/login', gaiaController.getLogin);
 router.get('/', auth, gaiaController.getProjects);
 router.get('/project/:prj', auth, gaiaController.getProject);
 router.get('/tasks/:prj', auth, gaiaController.getTasks);
-router.get('/tasksarea/:id', auth, gaiaController.getTasksarea);
 router.get('/users', auth, gaiaController.getUsers);
 router.get('/dashboard', auth, gaiaController.getDashboard);
 router.get('/import/:result', auth, gaiaController.getImport);
